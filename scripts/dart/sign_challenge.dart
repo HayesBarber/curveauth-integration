4ffa49007ae5678fd2b2293e539fd2151c59a5eb
Hayes Barber <barberhayes@gmail.com>
2025-07-30 20:08:29 -0400

    auto commit

--- scripts/dart/sign_challenge.dart
+++ scripts/dart/sign_challenge.dart
@@ -1,13 +1,24 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:curveauth_dart/curveauth_dart.dart';
 
 void main(List<String> args) async {
-  final json = args[0];
+  if (args.length != 2) {
+    print('Usage: dart sign_challenge.dart <keypair_json> <challenge>');
+    exit(1);
+  }
+
+  final jsonStr = args[0];
   final challenge = args[1];
 
-  final keyPair = ECCKeyPair.fromJson(jsonDecode(json));
+  late final ECCKeyPair keyPair;
+  try {
+    keyPair = ECCKeyPair.fromJson(jsonDecode(jsonStr));
+  } catch (e) {
+    print('Invalid keypair JSON: $e');
+    exit(1);
+  }
 
   final signature = await keyPair.createSignature(challenge);
-
   print(signature);
 }
